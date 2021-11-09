d50081fca1b19392784562cf296ad6476bfd69ad
Ivar Fernandez <ivarfermar7@gmail.com>
2021-11-09 10:57:24 +0100

    1847.03 Añade atributo paginas y getter

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int paginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroPaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        paginas = numeroPaginas;
     }
     
     public String getaAutor() {
@@ -28,6 +30,10 @@ public class Libro {
     
     public String getTitulo() {
         return titulo;
+    }
+    
+    public int getPaginas() {
+        return paginas;
     }
     
     public void imprimeAutor() {
